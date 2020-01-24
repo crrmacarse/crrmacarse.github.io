d2108a142c0059760e7216444ec0651960132aec
crrmacarse <crrmacarse@gmail.com>
2020-01-24 19:43:43 +0800

    Added fancy animation for homepage

--- src/components/public/home/index.tsx
+++ src/components/public/home/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useTrail, animated } from 'react-spring';
 import { useTranslation } from "react-i18next";
+
+const fast = { tension: 1200, friction: 40 }
+const slow = { mass: 10, tension: 200, friction: 50 }
+const trans = (x: number, y: number) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
 
 const Home = () => {
     const { t } = useTranslation();
+    const [trail, set] = useTrail<any>(3, () => ({ xy: [0, 0], config: (i: any) => (i === 0 ? fast : slow) }))
 
     return <div className="home-section flex flex-col items-center justify-center">
-        <h1 className="text-6xl">{t("home.title")}</h1>
-        <p className="text-sm">{t('home.subtitle')} <a href="https://crrmacarse.github.io/old-portfolio/" className="font-bold">{t('Click here')}</a></p>
-    </div>;
+        <svg style={{ position: 'absolute', width: 0, height: 0 }}>
+            <filter id="goo">
+                <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+                <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
+            </filter>
+        </svg>
+        <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}>
+            {trail.map((props: any, index) => (
+                <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
+            ))}
+        </div>
+        <div className="z-20 text-center cursor-default">
+            <h1>{t("home.title")}</h1>
+            <p className="text-lg">{t('home.subtitle')} <a href="https://crrmacarse.github.io/old-portfolio/" className="font-bold">{t('Click here')}</a></p>
+        </div>
+    </div>
 }
 
 export default Home;
