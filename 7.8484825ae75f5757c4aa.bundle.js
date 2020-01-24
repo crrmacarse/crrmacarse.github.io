8cf3c1fc8a1f64fbf9b6d5355aa50edf2e342019
crrmacarse <crrmacarse@gmail.com>
2020-01-24 14:09:47 +0800

    Updates

--- 7.8484825ae75f5757c4aa.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{48:function(e,t,a){"use strict";a.r(t);var c=a(0),s=a.n(c),l=a(3);t.default=()=>{const{t:e}=Object(l.b)();return s.a.createElement("div",{className:"home-section flex flex-col items-center justify-center"},s.a.createElement("h1",{className:"text-6xl"},e("home.title")),s.a.createElement("p",{className:"text-sm"},e("home.subtitle")," ",s.a.createElement("a",{href:"https://crrmacarse.github.io/old-portfolio/",className:"font-bold"},e("Click here"))))}}}]);
-//# sourceMappingURL=7.8484825ae75f5757c4aa.bundle.js.map
