911c8ff29f6752587f4d1a08afdcfa600d27bd6a
crrmacarse <crrmacarse@gmail.com>
2020-01-25 15:18:49 +0800

    Improved Error Handling

---
+++ src/hoc/toggler.tsx
@@ -0,0 +1,10 @@
+import React, { useState, FunctionComponent } from 'react';
+
+const Toggler = (WrappedComponent: FunctionComponent) => (props: any) => {
+    const [open, setOpen] = useState(false);
+    const handleToggle = () => setOpen(!open);
+
+    return <WrappedComponent {...props} isOpen={open} toggle={handleToggle} />
+}
+
+export default Toggler
