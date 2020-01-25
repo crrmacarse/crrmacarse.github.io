fb30b246fb442cc97212bf31b4d90bd5fe66b4ec
crrmacarse <crrmacarse@gmail.com>
2020-01-25 22:38:19 +0800

    Extended Testing and Refactored Prod Build

--- src/__tests__/components/navbar.test.tsx
+++ src/__tests__/components/navbar.test.tsx
@@ -6,4 +6,14 @@ const component = shallow(<Footer />);
 
 describe('<Footer />', () => {
   it('renders', () => { component; });
+
+  // it('renders children when passed in', () => {
+  //   const wrapper = shallow((
+  //     <div>
+  //       <div className="test" />
+  //     </div>
+  //   ));
+  //   expect(wrapper.contains(<div className="unique" />)).toBe(true);
+  // });
+
 });
