6f81242bcd9a2db070a2f282b5894c33875c31c4
crrmacarse <crrmacarse@gmail.com>
2020-01-26 04:19:29 +0800

    Updated 404 Page

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const NotFound = () => <div>Not Found</div>;
+const NotFound = () => <div className="not-found-section flex flex-col w-full h-screen justify-center items-center">
+    <p className="text-6xl">Page Not Found :(</p>
+    <a href="/" className="my-3 font-semibold">Return to the homepage.</a>
+</div>;
 
 export default NotFound;
--- src/utils/getCoordinates.ts
+++ src/utils/getCoordinates.ts
@@ -1,6 +1,7 @@
 export default () => {
   try {
     const geoLocation = navigator.geolocation;
+
     return new Promise((resolve, reject) => {
       geoLocation.getCurrentPosition(resolve, reject, {
         enableHighAccuracy: true,
