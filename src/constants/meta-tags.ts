9cb27a126937c880a02584c132528c19454b8685
crrmacarse <crrmacarse@gmail.com>
2020-01-26 19:54:05 +0800

    SEO Imporvments

---
+++ src/constants/meta-tags.ts
@@ -0,0 +1,32 @@
+import * as Routes from './routes';
+
+export default [
+  {
+    page: Routes.HOME,
+    description: 'Christian Ryan Macarse&apos;s website portfolio. A showcase of my capability and knowledge with web development. See for yourself! | Christian Ryan Macarse',
+  },
+  {
+    page: Routes.PORTFOLIO,
+    description: 'A Portfolio with list of my projects I&apos;ve done in solo and collaboration throughout my Software development years | Christian Ryan Macarse',
+  },
+  {
+    page: Routes.RESUME,
+    description: 'Checkout my update resume! I&apos;m open for opportunity for a much better full time job | Christian Ryan Macarse',
+  },
+  {
+    page: Routes.ABOUT,
+    description: 'A brief summary of myself pertaining to my career and personal life | Christian Ryan Macarse',
+  },
+  {
+    page: Routes.PERSONAL,
+    description: 'A personal page showcasing my career growth, blogs, and private life | Christian Ryan Macarse',
+  },
+  {
+    page: Routes.MORE,
+    description: 'More random stuff about Christian Ryan Macarse | Christian Ryan Macarse',
+  },
+  {
+    page: Routes.CONTACT,
+    description: 'A page displaying all my contact details. Reaching out to me made easy! | Christian Ryan Macarse',
+  },
+];
