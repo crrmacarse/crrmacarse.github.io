8064ca00b85dfa92625ca2237ea7d057d594a906
crrmacarse <crrmacarse@gmail.com>
2020-01-26 09:00:47 +0800

    Optimization Attempt

--- src/components/public/resume/index.tsx
+++ src/components/public/resume/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import { useTranslation } from 'react-i18next';
 import ResumePDF from 'assets/pdfs/resume.pdf';
 
