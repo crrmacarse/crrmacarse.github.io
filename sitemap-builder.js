3fb77274683270b4664c3e7e16b249044487d7f8
crrmacarse <crrmacarse@gmail.com>
2020-01-28 00:19:32 +0800

    Removed babel-node at .yml;  You need to manually build sitemap.xml if you want to change it in CD

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -3,13 +3,14 @@ const Sitemap = require('./node_modules/react-router-sitemap').default;
 const Router = require('./routes').default;
 
 const PUBLIC_URL = process.env.PUBLIC_URL || 'https://crrmacarse.github.io';
-const BUILD_PATH = './dist/sitemap.xml';
+// const BUILD_PATH = './dist/sitemap.xml';
+const MANUAL_BUILD_PATH = './sitemap.xml'; // https://babeljs.io/docs/en/next/babel-node.html#not-meant-for-production-use
 
 function generateSitemap() {
   try {
     return (new Sitemap(Router)
       .build(PUBLIC_URL)
-      .save(BUILD_PATH)
+      .save(MANUAL_BUILD_PATH)
     );
   } catch (error) {
     return console.warn('Something went wrong in building the sitemap', error);
