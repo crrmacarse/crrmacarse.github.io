824dbbcccf0e47d23a7ba4b19f0793c362372a6a
crrmacarse <crrmacarse@gmail.com>
2020-01-28 16:04:10 +0800

    Updates

---
+++ 17.5d02700ff4f3ed6ae504.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{549:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a);n.default=function(){return l.a.createElement("div",null,l.a.createElement("div",{className:"title"},l.a.createElement("h1",{className:"text-5xl"},"Trending News Right Now"),l.a.createElement("p",null,"Currently researching for a well documented news api")),l.a.createElement("div",{className:"content"}))}}}]);
+//# sourceMappingURL=17.5d02700ff4f3ed6ae504.bundle.js.map
