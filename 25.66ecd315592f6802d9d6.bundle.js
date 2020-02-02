2dcfb049ddb9ec7b4a7895096c03181f3988b1d2
crrmacarse <crrmacarse@gmail.com>
2020-02-02 16:43:28 +0000

    Deploying to master from  - 29a1761bfc4f73c48bc6b7e01b8d9cc003604736 🚀

---
+++ 25.66ecd315592f6802d9d6.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{570:function(e,n,t){"use strict";t.r(n);var c=t(0),a=t.n(c);n.default=function(){return a.a.createElement("div",{className:"admin-section flex items-center h-screen justify-center flex-col"},a.a.createElement("h1",null,"ADMIN BLOG EDIT"))}}}]);
+//# sourceMappingURL=25.66ecd315592f6802d9d6.bundle.js.map
