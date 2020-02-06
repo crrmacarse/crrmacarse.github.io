a3302892a58caad06757ce444ef47d11413e24be
crrmacarse <crrmacarse@gmail.com>
2020-02-06 15:17:43 +0000

    Deploying to master from  - e6c1213d0324a2dba507fe95b1a59e2eefd330d5 🚀

---
+++ 24.f5821af249c2f2fd34c9.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{572:function(e,n,t){"use strict";t.r(n);var c=t(0),a=t.n(c);n.default=function(){return a.a.createElement("div",{className:"admin-section flex items-center h-screen justify-center flex-col"},a.a.createElement("h1",null,"ADMIN BLOG ADD"))}}}]);
+//# sourceMappingURL=24.f5821af249c2f2fd34c9.bundle.js.map
