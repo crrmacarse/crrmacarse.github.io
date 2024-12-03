76e3242492656a0e6389f2ef657561270d3aca14
crrmacarse <crrmacarse@gmail.com>
2024-12-04 06:58:38 +0800

    feat: updated root

--- v2/app.tsx
+++ v2/app.tsx
@@ -1,15 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import * as ROUTES from '@v2/constants/routes';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import * as PAGE_ROUTES from '@v2/constants/routes';
 import '@v2/app.css';
+import '@v2/firebase';
+// TODO: Not working
+// import '@v2/scss/index.scss'
 import '@v2/i18n';
-import Main from '@v2/components/main';
-import Footer from '@v2/components/footer';
 import useDarkMode from "@v2/hooks/useDarkMode";
 import Navbar from '@v2/components/navbar';
-// TODO: Asset import
-import ImageSrc from '../essentials/assets/kitanglad.JPG';
-import PortfolioPage from './components/portfolio';
+import Footer from '@v2/components/footer';
+import HomePage from '@v2/components/home';
+import PortfolioPage from '@v2/components/portfolio';
+import ResumePage from '@v2/components/resume';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -17,19 +19,17 @@ const root = createRoot(container);
 const App = () => {
   const { theme, isFirstTime, toggleTheme } = useDarkMode();
 
-
   return (
-    <div className={`theme theme--${theme} bg-cover bg-no-repeat bg-center	`} style={{ backgroundImage: `url(${ImageSrc})` }}>
-      {/* <img src={ImageSrc} /> */}
-      <Router>
-        <Navbar isFirstTime={isFirstTime} theme={theme} toggleTheme={toggleTheme} visited={true} />
+    <div className={`theme theme--${theme}`}>
+      <BrowserRouter>
+      <Navbar isFirstTime={isFirstTime} theme={theme} toggleTheme={toggleTheme} visited={true} />
         <Routes>
-          <Route path="/" element={<Main />} />
-          {/* TODO: Not working */}
-          <Route path={ROUTES.PORTFOLIO} element={<PortfolioPage />} />
+          <Route path={PAGE_ROUTES.HOME} element={<HomePage />} />
+          <Route path={PAGE_ROUTES.PORTFOLIO} element={<PortfolioPage />} />
+          <Route path={PAGE_ROUTES.RESUME} element={<ResumePage />} />
         </Routes>
-        <Footer />
-      </Router>
+      <Footer />
+      </BrowserRouter>
     </div>
   );
 };
