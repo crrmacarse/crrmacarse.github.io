51f13cb596b1649824e1e186458b9be27df8621f
crrmacarse <crrmacarse@gmail.com>
2024-12-04 06:51:45 +0800

    feat: added projects data

---
+++ v2/constants/projects.ts
@@ -0,0 +1,98 @@
+export const PROJECTS_DATA = [
+  {
+    description: "A website project for SimpleWaste",
+    image: { alt: 'Simplewaste Website', src: '/assets/images/projects/simplewaste-web.png' },
+    links: { website: 'https://simplewaste.com' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste Web",
+    priority: 1,
+  },
+  {
+    description: "Company website for Cogitate",
+    image: { alt: 'Cogitate Website', src: '/assets/images/projects/cogitate-web.png' },
+    links: { website: 'https://cogitate.io' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "Cogitate",
+    priority: 1,
+  },
+  {
+    description: "An e-commerce website for Harmony Public Schools. One of the largest public school system in the US",
+    image: { alt: 'Harmony Public Schools', src: '/assets/images/projects/harmony-public-schools.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss', 'shopify', 'aws'],
+    title: "Harmony Public Schools",
+    priority: 1,
+  },
+  {
+    description: "Bidding App for SimpleWaste",
+    image:
+      { alt: 'SimpleWaste Bidding', src: '/assets/images/projects/simplewaste-bidding.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste Bidding",
+    priority: 2,
+  },
+  {
+    description: "Centralized API system for SimpleWaste",
+    image:
+      { alt: 'SimpleWaste API', src: '/assets/images/projects/simplewaste-api.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nestjs', 'postgres', 'aws', 'googlecloud'],
+    title: "SimpleWaste API",
+    priority: 2,
+  },
+  {
+    description: "Positive Kiosk",
+    image:
+      { alt: 'Positive Kiosk', src: '/assets/images/projects/positive-kiosk.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['react', 'tailwindcss', 'calendly'],
+    title: "Positive Kiosk",
+    priority: 5,
+  },
+  {
+    description: "Status hub for SimpleWaste",
+    image:
+      { alt: 'SimpleWaste Status Hub', src: '/assets/images/projects/simplewaste-statushub.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste Status Hub",
+    priority: 6,
+  },
+  {
+    description: "PartnerSync App for SimpleWaste",
+    image:
+      { alt: 'SimpleWaste PartnerSync', src: '/assets/images/projects/simplewaste-partnersynchub.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste PartnerSync",
+    priority: 7,
+  },
+  {
+    description: "Oversight App for SimpleWaste",
+    image:
+      { alt: 'SimpleWaste Oversight', src: '/assets/images/projects/simplewaste-oversight.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste Oversight",
+    priority: 8,
+  },
+  {
+    description: "Onboarding App for SimpleWaste",
+    image:
+      { alt: 'SimpleWaste Onboarding', src: '/assets/images/projects/simplewaste-onboarding.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste Onboarding",
+    priority: 9
+  },
+  {
+    description: "SimpleWaste Vendor Signup Website",
+    image:
+      { alt: 'SimpleWaste Vendor Signup Website', src: '/assets/images/projects/simplewaste-vendorsignup.png' },
+    links: { website: 'https://careers.xtendops.com/' },
+    tags: ['nextjs', 'react', 'tailwindcss'],
+    title: "SimpleWaste Vendor Signup Website",
+    priority: 10,
+  },
+]
