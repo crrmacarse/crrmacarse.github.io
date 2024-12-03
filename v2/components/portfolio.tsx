356e00fedfd00f1032c25f06b3782b9723c79c25
crrmacarse <crrmacarse@gmail.com>
2024-12-04 06:54:32 +0800

    feat: added portfolio page

--- v2/components/portfolio.tsx
+++ v2/components/portfolio.tsx
@@ -1,14 +1,24 @@
 import orderBy from 'lodash/orderBy';
 import Card from './card';
+import db from '@v2/firebase/db';
+import { useEffect, useState } from 'react';
+import { PROJECTS_DATA } from '@v2/constants/projects';
 
 // @TODO:
 // add filter here to personal and company related projects
-// add pagination?
-const PortfolioPage = () => (
-    <div className="portfolio-section">
-      <ul className="portfolio-section__grid w-11/12 mx-auto">
-        {true && <p>Fetching...</p>}
-        {orderBy([], 'priority').map((project: any, key: number) => (
+const PortfolioPage = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    db.ref('/projects').once('value').then((snapshot) => {
+      setProjects([...PROJECTS_DATA, ...snapshot.val()]);
+    });
+  }, [])
+
+  return (
+    <div className="portfolio-section min-h-screen mt-36">
+      <ul className="w-11/12 mx-auto grid-cols-3 grid gap-5">
+        {orderBy(projects, 'priority').map((project: any, key: number) => (
           <li key={key} className="lg:mx-auto">
             <Card {...project} />
           </li>
@@ -16,5 +26,6 @@ const PortfolioPage = () => (
       </ul>
     </div>
   );
+};
 
 export default PortfolioPage;
