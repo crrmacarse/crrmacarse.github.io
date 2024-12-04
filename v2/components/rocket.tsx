950063fb6449a759ffb9557b70f518c317af88d0
crrmacarse <crrmacarse@gmail.com>
2024-12-05 01:43:48 +0800

    feat: added rocket reset and idle note

--- v2/components/rocket.tsx
+++ v2/components/rocket.tsx
@@ -1,21 +1,27 @@
 import { useSpring, to, animated, config } from '@react-spring/web'
 import { scale, dist } from 'vec-la'
 import { useDrag } from 'react-use-gesture'
+import { useCallback, useEffect, useState } from 'react'
 
 // TODO: Adjust zindex
 const Rocket = () => {
+  type trackType = "initial" | "idle" | "moved"
+  const [track, setTrack] = useState<trackType>('initial');
+
   const [{ pos }, api] = useSpring(() => ({ pos: [0, 0] }))
   const [{ angle }, angleApi] = useSpring(() => ({
     angle: 0,
     config: config.wobbly,
   }))
+
   // direction calculates pointer direction
   // memo is like a cache, it contains the values that you return inside "set"
   // this way we can inject the springs current coordinates on the initial event and
   // add movement to it for convenience
-
   const bind = useDrag(
     ({ xy, previous, down, movement: pos, velocity, direction }) => {
+      setTrack("moved")
+
       api.start({
         pos,
         immediate: down,
@@ -28,8 +34,23 @@ const Rocket = () => {
     { initial: () => pos.get() }
   )
 
+  const handleReset = () => {
+    api.set({ pos: [0, 0] })
+    setTrack("initial")
+  }
+
+  useEffect(() => {
+    if(track !== 'initial') return;
+
+    const timer = setTimeout(() => {
+      setTrack('idle')
+    }, 5000)
+
+    return () => clearTimeout(timer);
+  }, [track])
+
   return (
-    <div>
+    <p className='text-center mb-2'>
       <animated.div
         {...bind()}
         style={{
@@ -39,14 +60,15 @@ const Rocket = () => {
             ([x, y], a) => `translate3d(${x}px,${y}px,0) rotate(${a}rad)`
           ),
         }}>
-        <img src={"/assets/images/rocket.gif"} className='rocket' width={200} height={200} draggable="false" style={{
+        <img src={"/assets/images/rocket.gif"} className='rocket mx-auto' width={200} height={200} draggable="false" style={{
           cursor: '-webkit-grab',
           willChange: 'transform',
           userSelect: 'none',
         }} />
       </animated.div>
-      {/* <small onClick={() => {}}>(Rocket lost? Click here)</small> */}
-    </div>
+      <p className={`text-xs blink ${track === 'idle' ? 'visible' : 'invisible'}`}>(Drag me!)</p>
+      <p className={`text-xs ${track === 'moved' ? 'visible' : 'invisible'}`}>(Rocket lost? <button onClick={handleReset}>Click here</button>)</p>
+    </p>
   )
 }
 
