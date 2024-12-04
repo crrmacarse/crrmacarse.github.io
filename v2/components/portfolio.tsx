c01983ce847afb7b1a7ade29958dec859219e870
crrmacarse <crrmacarse@gmail.com>
2024-12-04 18:22:16 +0800

    feat: added tag filter

--- v2/components/portfolio.tsx
+++ v2/components/portfolio.tsx
@@ -3,22 +3,51 @@ import Card from './card';
 import db from '@v2/firebase/db';
 import { useEffect, useState } from 'react';
 import { PROJECTS_DATA } from '@v2/constants/projects';
+import Tags, { tagType } from './tags';
+import { isEmpty } from 'lodash';
 
-// @TODO:
-// add filter here to personal and company related projects
 const PortfolioPage = () => {
   const [projects, setProjects] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const selectedTags = tags.filter((t: tagType) => t.isSelected).map((t) => t.title);
+  const filteredProjects = isEmpty(selectedTags) ? projects : projects.filter((p) => p.tags.some((t: string) => selectedTags.includes(t)))
+
+  const handleTagClick = (tag: tagType) => {
+    const updatedTags = tags.map((t) => {
+      const updatedTag = t.title === tag.title && { isSelected: !t.isSelected };
+
+      return {
+        ...t,
+        ...updatedTag,
+      }
+    })
+
+    setTags(updatedTags);
+  }
+
+  const handleTagReset = () => {
+    const updatedTags = tags.map((t) => ({ ...t, isSelected: false }));
+
+    setTags(updatedTags);
+  }
 
   useEffect(() => {
     db.ref('/projects').once('value').then((snapshot) => {
-      setProjects([...PROJECTS_DATA, ...snapshot.val()]);
+      const data = [...PROJECTS_DATA, ...snapshot.val()];
+      const dataTags = data.map((d) => d.tags).reduce((acc, val) => acc.concat(val), []);
+      const dataTagsFinal = [...new Set(dataTags)].map((v) => { return { title: v, isSelected: false }});
+
+      setTags(dataTagsFinal)
+      setProjects(data);
     });
   }, [])
 
   return (
-    <div className="portfolio-section min-h-screen mt-36">
+    <div className="portfolio-section min-h-screen pt-28">
+      <Tags tags={tags} onSelect={(t) => handleTagClick(t)} onReset={handleTagReset}/>
       <ul className="w-11/12 mx-auto grid-cols-3 grid gap-5">
-        {orderBy(projects, 'priority').map((project: any, key: number) => (
+        {orderBy(filteredProjects, 'priority').map((project: any, key: number) => (
           <li key={key} className="lg:mx-auto">
             <Card {...project} />
           </li>
