2eba83fc6f8fbc7186b7a20eb61bab91af9d905b
crrmacarse <crrmacarse@gmail.com>
2025-02-11 11:55:46 +0800

    feat: updated resume link

--- v2/components/resume.tsx
+++ v2/components/resume.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
-const GOOGLE_DOCS_LINK = 'https://drive.google.com/file/d/1NozqdnWpFYNkGeq8nS08Rpde0o75ClFn';
+const GOOGLE_DOCS_LINK = 'https://drive.google.com/file/d/1_FB2OYWS_M9x3gVthZREYOw8pqLw00Xi';
 const DOWNLOADABLE_LINK = `${GOOGLE_DOCS_LINK}/export?format=pdf`.trim();
 
 const ResumePage = () => {
@@ -16,7 +16,7 @@ const ResumePage = () => {
         <Page width={900} pageNumber={1} renderAnnotationLayer={false} renderTextLayer={false} />
       </Document> */}
       {/* <p className="lg:hidden text-center">Sorry, PDF Viewer doesn&apos;t work in mobile.</p> */}
-      <iframe src="https://drive.google.com/file/d/1NozqdnWpFYNkGeq8nS08Rpde0o75ClFn/preview" width="90%" height="900" allow="autoplay" className='mb-8'></iframe>
+      <iframe src={`${GOOGLE_DOCS_LINK}/preview`} width="90%" height="900" allow="autoplay" className='mb-8'></iframe>
       <div className="footer font-bold text-lg text-center">
         <a href={GOOGLE_DOCS_LINK} className="md:mx-8 mx-3">{t('View Google Docs Version')}</a>
         <a href={DOWNLOADABLE_LINK} className="block md:inline lg:mx-8 mx-3">{t('Download')}</a>
